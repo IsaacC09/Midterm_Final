7f042d4631ccf6fc7d3a0f0216c46f6c2a25ec02
Isaac <Isaac@Isaac.hsd1.de.comcast.net>
2016-10-16 23:40:44 -0400

    commit

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,20 +15,79 @@ import com.cisc181.eNums.eTitle;
 public class Staff_Test {
 
 	@BeforeClass
-	public static void setup() {
-		public Staff(String FirstName, String MiddleName, String LastName,
-				Date DOB, String Address, String Phone_number, String Email,
-				String officeHours, int rank, double salary, Date hireDate,
-				eTitle Title) 
-		Staff Amy = new Staff("Audrey" , "B", "Chouri", 
-				(99,5,12), "Hello Street", "(111)-111-1111", "a@yahoo.com"
-				"Mon 5-9", 5, 1.00, (16,5,12), MR);
+	public static void setup() throws PersonException {
 		
+		Date today = new Date();
+		
+		 ArrayList<Staff> staffers = new ArrayList<Staff>();
+		Staff A = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 1.00, today, eTitle.MRS);
+		Staff B = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 2.00, today, eTitle.MRS);
+		Staff C = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 3.00, today, eTitle.MRS);
+		Staff D = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 4.00, today, eTitle.MRS);
+		Staff E = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 5.00, today, eTitle.MRS);
+		staffers.add(A);
+		staffers.add(B);
+		staffers.add(C);
+		staffers.add(D);
+		staffers.add(E);
 	}
-	
+
 	@Test
-	public void test() {
-		assertEquals(1,1);
-	}	
+	public void testAvgSaleery() throws PersonException {
+		Date today = new Date();
+		
+		 ArrayList<Staff> staffers = new ArrayList<Staff>();
+		Staff A = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 1.00, today, eTitle.MRS);
+		Staff B = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 2.00, today, eTitle.MRS);
+		Staff C = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 3.00, today, eTitle.MRS);
+		Staff D = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 4.00, today, eTitle.MRS);
+		Staff E = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "(111)-111-1111", 
+				"a@yahoo.com","Mon 5-9", 5, 5.00, today, eTitle.MRS);
+		staffers.add(A);
+		staffers.add(B);
+		staffers.add(C);
+		staffers.add(D);
+		staffers.add(E);
+		double sum = 0;
+		for (Staff d : staffers) {
+			sum += d.getSalary();
+
+		}
+		 double average= sum/(staffers.size());
+		 assertEquals(average, 3.0,0);
+
+	}
+
+
+
+@Test(expected = PersonException.class)
+	public void testDOBException() throws PersonException{
+	
+	Date today = new Date((long) -2184022.849*1000*1000);
+	Person one = new Staff("Audrey" , "B", "Chouri", java.sql.Date.valueOf("1916-5-10"), "Hello Street", "(111)-111-1111", 
+		"a@yahoo.com","Mon 5-9", 5, 1.00, today, eTitle.MRS);
+
+	}
+
+
+
+@Test(expected = PersonException.class)
+	public void testPhoneException()throws PersonException {
+
+	Date today = new Date(1476658856);
+	Person one = new Staff("Audrey" , "B", "Chouri", today, "Hello Street", "111-111-1111", 
+		"a@yahoo.com","Mon 5-9", 5, 1.00, today, eTitle.MRS);
+		one.setPhone("1234567890");
+
+	}
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -4,18 +4,102 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.cisc181.eNums.eMajor;
+
 public class Student_Test {
-  
+	
+
 	@BeforeClass
-	public static void setup() {
+	public static  void setup() throws PersonException{
+		ArrayList<Course> courses = new ArrayList<Course>();
+		//(UUID courseID, String courseName, int gradePoints, eMajor major)
+		Course bio = new Course(UUID.randomUUID(), "Biology", 3, eMajor.NURSING);
+		Course chem = new Course(UUID.randomUUID(), "Chemistry", 3, eMajor.CHEM);
+		Course econ = new Course(UUID.randomUUID(), "Econ", 3, eMajor.BUSINESS);
+		courses.add(bio);
+		courses.add(chem);
+		courses.add(econ);
+		
+		ArrayList<Semester> semester = new ArrayList<Semester>();
+		//Semester(UUID semesterID, Date startDate, Date endDate)
+		Semester fall = new Semester(UUID.randomUUID(), java.sql.Date.valueOf("2016-08-10"),
+				java.sql.Date.valueOf("2016-12-10") );
+		Semester spring = new Semester(UUID.randomUUID(), java.sql.Date.valueOf("2017-2-10"),
+				java.sql.Date.valueOf("2017-5-10") );
+		semester.add(fall);
+		semester.add(spring);
+		
+		ArrayList<Section> sections = new ArrayList<Section>();
+		//Section(UUID courseID, UUID semesterID, UUID sectionID, int roomID)
+		Section bioFall = new Section(bio.getCourseID(),fall.getSemesterID(),UUID.randomUUID(),101);
+		Section chemFall = new Section(chem.getCourseID(),fall.getSemesterID(),UUID.randomUUID(),102);
+		Section econFall = new Section(econ.getCourseID(),fall.getSemesterID(),UUID.randomUUID(),103);
+		Section bioSpring = new Section(bio.getCourseID(),spring.getSemesterID(),UUID.randomUUID(),104);
+		Section chemSpring = new Section(chem.getCourseID(),spring.getSemesterID(),UUID.randomUUID(),105);
+		Section econSpring = new Section(econ.getCourseID(),spring.getSemesterID(),UUID.randomUUID(),106);
+		sections.add(bioFall);
+		sections.add(chemFall);
+		sections.add(econFall);
+		sections.add(bioSpring); 
+		sections.add(chemSpring);
+		sections.add(econSpring);
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		//Student(String FirstName, String MiddleName, String LastName,Date DOB, eMajor Major,
+		//String Address, String Phone_number, String Email)
+		
+		Student a = new Student("Amy", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student b = new Student("Blake", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.NURSING, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student c = new Student("Chris", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.CHEM, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student d = new Student("Dylan", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.PHYSICS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student e = new Student("Erin", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.COMPSI, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student f = new Student("Frank", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student g = new Student("George", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student h = new Student("Henry", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student i = new Student("Isaac", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		Student j = new Student("Jill", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		students.add(a);
+		students.add(b);
+		students.add(c);
+		students.add(d);
+		students.add(e);
+		students.add(f);
+		students.add(g);
+		students.add(h);
+		students.add(i);
+		students.add(j);
 	}
 
 	@Test
-	public void test() {
-		assertEquals(1, 1);
+	public void gradetest() throws PersonException{
+		Student a = new Student("Amy", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		a.setMajor(eMajor.CHEM);
+	}	
+	
+
+	@Test
+	public void changeMajortest() throws PersonException{
+		Student a = new Student("Amy", "B", "Rickman",java.sql.Date.valueOf("1995-12-10"), eMajor.BUSINESS, 
+				"Hello Street", "(302)-123-4567", "thismidtermsucks@udel.edu");
+		a.setMajor(eMajor.CHEM);
 	}
+	
+	
 }
